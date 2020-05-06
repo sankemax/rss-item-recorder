57d7a336d18661303d45582dfb14c9b681df190d
sankemax <sankeman2004@gmail.com>
2020-05-06 16:29:25 +0300

    add sentry to the mix

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const config = require('config');
 const cors = require('cors');
+const Sentry = require('@sentry/node');
+
+Sentry.init({ dsn: 'https://057bdd5dc4484dfd837832e1e8a44b5e@o388622.ingest.sentry.io/5225736' });
 
 const { initDb } = require('./repository/core');
 const { initReader } = require('./rssReader');
 const readerRouter = require('./router/readerRouter');
 const { sigs } = require('./utils/operations');
-
 const port = config.get('portForApi');
 
 initDb(config.get('dbPath'));
@@ -15,14 +17,21 @@ initReader();
 
 express()
     .use(cors())
+    .use(Sentry.Handlers.requestHandler())
+
     .get('/', (req, res) => res.send('Rss Item Recorder is alive!'))
+    .get('/debug-sentry', function mainHandler(req, res) {
+        throw new Error('My first Sentry error!');
+    })
+    
     .use('/api', readerRouter)
+    .use(Sentry.Handlers.errorHandler())
     .use((err, req, res, next) => {
         console.error(err.stack);
         if (res.headersSent) {
             return next(err);
         }
-        res.json({ success: false, message: err.message });
+        res.json({ success: false, message: res.sentry });
     })
     .listen(port, () => console.log(`rss recorder API is listening on port ${port}`));
 
