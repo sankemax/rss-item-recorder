a3d6e6efd13888de12107541154fa603a35bdbb1
sankemax <sankeman2004@gmail.com>
2020-05-27 18:37:56 +0300

    try different variation of tryCatch that may work with babel transpiling

--- src/utils/flowControl.js
+++ src/utils/flowControl.js
@@ -1,15 +1,17 @@
 module.exports = function tryCatch(fn) {
     return async function withFn(...args) {
-        if (fn.constructor.name === "AsyncFunction") {
-            return await fn(...args)
-                .then(ans => ({ ans }))
-                .catch(error => ({ error }));
-        }
-
         try {
-            return Promise.resolve({ ans: fn(...args) });
+            const maybePromise = fn(...args);
+            if (maybePromise.then && maybePromise.catch) {
+                return await maybePromise
+                    .then(ans => ({ ans }))
+                    .catch(error => ({ error }));
+
+            }
+            return Promise.resolve({ ans: maybePromise });
         } catch (error) {
             return Promise.resolve({ error });
         }
+
     }
 }
